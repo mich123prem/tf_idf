cf7992a3ca8bf406f1ca81c93d22a57a440b5609
michaelprem <michaelp@hioa.no>
2020-01-26 19:37:28 +0100

    Example assignment for MBIB4230 students

---
+++ README.md
@@ -0,0 +1,6 @@
+# TFIDF and cosine similarity - toy example
+#### By Michael Preminger
+#### Inspired by, and partly taken from the contributions of <a href="https://markhneedham.com/blog/2016/07/27/scitkit-learn-tfidf-and-cosine-similarity-for-computer-science-papers/">Mark Needham</a>  and <a href="https://towardsdatascience.com/tf-idf-for-document-ranking-from-scratch-in-python-on-real-world-dataset-796d339a4089">William Scott</a>
+
+### This Notebook demonstrate the use of TFIDF in retrieval. <br> ~6000 very short documents (stored in the papers/ directory) are read into memory, preprocessed to various degrees and are indexed for retrieval.<br> 
+#### A toy of a toy (10 documents) are available in the directory <b>papers1/</b>
---
+++ preprocess.py
@@ -0,0 +1,59 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from collections import Counter
+from num2words import num2words
+
+import nltk
+import os
+import string
+import numpy as np
+import copy
+import pandas as pd
+import pickle
+import re
+import math
+
+def convert_lower_case(data):
+    return np.char.lower(data)
+
+def remove_stop_words(data):
+    stop_words = stopwords.words('english')
+    words = word_tokenize(str(data))
+    new_text = ""
+    for w in words:
+        if w not in stop_words and len(w) > 1:
+            new_text = new_text + " " + w
+    return new_text
+
+def remove_punctuation(data):
+    symbols = "!\"#$%&()*+-./:;<=>?@[\]^_`{|}~\n"
+    for i in range(len(symbols)):
+        data = np.char.replace(data, symbols[i], ' ')
+        data = np.char.replace(data, "  ", " ")
+    data = np.char.replace(data, ',', '')
+    return data
+
+def remove_apostrophe(data):
+    return np.char.replace(data, "'", "")
+
+def stemming(data):
+    stemmer= PorterStemmer()
+    
+    tokens = word_tokenize(str(data))
+    new_text = ""
+    for w in tokens:
+        new_text = new_text + " " + stemmer.stem(w)
+    return new_text
+
+def convert_numbers(data):
+    tokens = word_tokenize(str(data))
+    new_text = ""
+    for w in tokens:
+        try:
+            w = num2words(int(w))
+        except:
+            a = 0
+        new_text = new_text + " " + w
+    new_text = np.char.replace(new_text, "-", " ")
+    return new_text
